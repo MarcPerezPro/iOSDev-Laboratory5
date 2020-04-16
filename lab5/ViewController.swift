04b7ebc64d4d4a658a456ee156fd734598ad7adf
Marc PEREZ <marc.perez@epitech.eu>
2020-04-17 00:57:51 +0200

    Added the logic, it works on the first try

--- lab5/ViewController.swift
+++ lab5/ViewController.swift
@@ -10,28 +10,69 @@ import UIKit
 import WebKit
 
 class ViewController: UIViewController, WKUIDelegate {
+    
+    let ALERT_EMPTY_URL = "You must provide some URL to go!"
+    let ALERT_INVALID_URL = "You must provide a valid URL to go!"
 
     @IBOutlet weak var webView: WKWebView!
     @IBAction func goBackButton(_ sender: UIButton) {
+        webView.goBack()
     }
     @IBAction func goForwardButton(_ sender: UIButton) {
+        webView.goForward()
     }
     @IBAction func goHomeButton(_ sender: UIButton) {
+        goHome()
     }
     @IBOutlet weak var searchBar: UITextField!
+    
+    func showAlert(message: String) {
+        let alertController = UIAlertController(title: "ERROR", message:
+            message, preferredStyle: .alert)
+        alertController.addAction(UIAlertAction(title: "OK", style: .default))
+        self.present(alertController, animated: true, completion: nil)
+    }
+    
     @IBAction func goToURLButton(_ sender: UIButton) {
+        var text = searchBar.text
+        if (text == nil || text!.isEmpty) {
+            showAlert(message: ALERT_EMPTY_URL)
+            return
+        }
+        let https = "https://"
+        if (!text!.starts(with: https)) {
+            text = https + text!
+        }
+        goToURL(urlString: text)
     }
     @IBOutlet weak var contentSecureLabel: UILabel!
+    
+    func goToURL(url: URL) {
+        let myRequest = URLRequest(url: url)
+        webView.load(myRequest)
+    }
+    func goToURL(urlString: String?) {
+        guard urlString != nil else {
+            showAlert(message: ALERT_EMPTY_URL)
+            return
+        }
+        let url = URL(string: urlString!)
+        if (url == nil) {
+            showAlert(message: ALERT_INVALID_URL)
+            return
+        }
+        goToURL(url: url!)
+    }
+
+    func goHome() {
+        goToURL(urlString: "https://www.google.com")
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         webView.uiDelegate = self
-        let myURL = URL(string:"https://www.apple.com")
-        let myRequest = URLRequest(url: myURL!)
-        webView.load(myRequest)
+        goHome()
     }
-
-
 }
 
