cfcc7c34abbfb2c941a5c2c028de365788bb57ce
Marc PEREZ <marc.perez@epitech.eu>
2020-04-16 23:40:49 +0200

    Initial Commit

---
+++ lab5/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  lab5
+//
+//  Created by Marc PEREZ on 16/4/2020.
+//  Copyright © 2020 Marc PEREZ. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
